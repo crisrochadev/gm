bdaf66af4c88be37b043372b5056c98c330e5c74
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-12-26 09:07:55 -0300

    change styles

--- cdn/main.js
+++ cdn/main.js
@@ -20,10 +20,10 @@ $(function loadImg() {
   let swiper = new Swiper(".mySwiper", {
   
     spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
